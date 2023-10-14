2378596610af4fbce9f69c6cba19cfdbcbafa447
Thomas Schoffelen <thomas@schof.co>
2023-10-14 12:57:34 +0100

    feat: base url on current editor

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,21 @@
-import { ExtensionContext, Uri, commands } from "vscode";
+import { ExtensionContext, Uri, TextDocument, commands, window } from "vscode";
 import duplicate from "./commands/duplicate";
 
 export async function activate(context: ExtensionContext) {
-    context.subscriptions.push(
-        commands.registerCommand("file-duplicate.execute", (uri: Uri) =>
-            duplicate(uri)
-        )
-    );
+  context.subscriptions.push(
+    commands.registerCommand(
+      "file-duplicate.execute",
+      (uri: TextDocument | Uri) => {
+        if (!uri || !(<Uri>uri).fsPath) {
+          const editor = window.activeTextEditor;
+          if (!editor) return;
+          return duplicate(<Uri>editor.document.uri);
+        }
+
+        duplicate(<Uri>uri);
+      }
+    )
+  );
 }
 
 export function deactivate() {}
