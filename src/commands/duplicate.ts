8906b02c982ac72be80664c51e64ac8495f278af
Thomas Schoffelen <thomas@schof.co>
2023-10-14 12:45:39 +0100

    fix: don't require actions to succeed

--- src/commands/duplicate.ts
+++ src/commands/duplicate.ts
@@ -2,47 +2,55 @@ import { basename, dirname } from "path";
 import { Uri, commands, window, workspace } from "vscode";
 
 function getCopyName(original: string, attempt: number): string {
-    let lastIndex = original.lastIndexOf(".");
-    if (original.includes(".test.")) {
-        lastIndex -= 5;
-    }
+  let lastIndex = original.lastIndexOf(".");
+  if (original.includes(".test.")) {
+    lastIndex -= 5;
+  }
 
-    let name = lastIndex === -1 ? original : original.slice(0, lastIndex);
-    let ext = lastIndex === -1 ? "" : original.slice(lastIndex);
+  let name = lastIndex === -1 ? original : original.slice(0, lastIndex);
+  let ext = lastIndex === -1 ? "" : original.slice(lastIndex);
 
-    name += "-copy";
-    name += attempt ? attempt + 1 : "";
-    name += ext;
+  name += "-copy";
+  name += attempt ? attempt + 1 : "";
+  name += ext;
 
-    return name;
+  return name;
 }
 
 async function executeCopy(uri: Uri, attempt: number = 0) {
-    const { fsPath } = uri;
-    const file = basename(fsPath);
-    const copyName = getCopyName(file, attempt);
+  const { fsPath } = uri;
+  const file = basename(fsPath);
+  const copyName = getCopyName(file, attempt);
 
-    const directory = Uri.file(dirname(fsPath));
-    const oldFile = Uri.file(fsPath);
-    const newFile = Uri.joinPath(directory, copyName);
+  const directory = Uri.file(dirname(fsPath));
+  const oldFile = Uri.file(fsPath);
+  const newFile = Uri.joinPath(directory, copyName);
 
-    await workspace.fs.copy(oldFile, newFile);
+  await workspace.fs.copy(oldFile, newFile);
 
+  try {
     await window.showTextDocument(newFile);
+  } catch (e) {}
+  try {
     await commands.executeCommand("revealInExplorer");
+  } catch (e) {}
+  try {
     await commands.executeCommand("renameFile");
+  } catch (e) {}
 }
 
 export default async function duplicate(uri: Uri) {
-    let attempt = 0;
-    while (attempt < 10) {
-        try {
-            await executeCopy(uri, attempt);
-            return;
-        } catch (error) {
-            attempt++;
-        }
+  let attempt = 0;
+  let err = null;
+  while (attempt < 10) {
+    try {
+      await executeCopy(uri, attempt);
+      return;
+    } catch (error) {
+      attempt++;
+      err = error?.message || error?.toString();
     }
+  }
 
-    window.showErrorMessage("Refusing to overwrite existing copy!");
+  window.showErrorMessage("Refusing to overwrite existing copy: " + err);
 }
